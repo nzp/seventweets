8060efee71eaa1c1ca2ef58e7fdc6684614d062c
Nikola Pavlović <nzp@riseup.net>
2017-05-12 01:54:34 +0200

    Rewrite storage.Tweet for db storage.
    It will be needed in storage functions to come later.  Rewrite the
    tests.

--- seventweets/storage.py
+++ seventweets/storage.py
@@ -1,60 +1,77 @@
 import json
 import itertools
+import functools
+
+import pg8000
+
 
 NODE_NAME = 'nzp'
 
 
 class Tweet:
-    _id_counter = None
-
-    @classmethod
-    def reset_counter(cls):
-        cls._id_counter = itertools.count()
 
     def __init__(self, tweet):
-        self.id = next(self._id_counter)
-        self.name = NODE_NAME
-        self.tweet = tweet
+        """Initialize a tweet object from the list returned by pg8000."""
+
+        self.id = tweet[0]
+        self.name = tweet[1]
+        self.tweet = tweet[2]
 
     def __str__(self):
         return self.tweet
 
+    def jsoned(self):
+        return json.dumps({'id': self.id, 'name': self.name, 'tweet': self.tweet})
 
-Tweet.reset_counter()
 
-
-class Storage:
-    # Id indexed dictionary of tweets (see save_tweet method).  Maybe overkill,
-    # but a lot faster in general than searching a list of Tweet objects for
-    # the wanted id.
-    _tweets = {}
-
-    @classmethod
-    def save_tweet(cls, tweet):
-        t = Tweet(tweet)
-        cls._tweets[t.id] = t
-
-        return json.dumps(t.__dict__)
-
-    @classmethod
-    def get_all(cls):
-        return json.dumps([t.__dict__ for t in cls._tweets.values()])
-
-    @classmethod
-    def get_tweet(cls, id):
-        try:
-            tweet = json.dumps(cls._tweets[id].__dict__)
-        except KeyError:
-            tweet = None
-
-        return tweet
-
-    @classmethod
-    def delete_tweet(cls, id):
-        tweet_id = cls._tweets.pop(id, None)
-
-        if tweet_id or tweet_id == 0:
-            return True
-        else:
-            return False
-
+#def uses_db(f):
+#    @functools.wraps(f)
+#    def wrapper(cls, *args, **kwargs):
+#        cursor = cls._connection.cursor()
+#        res = f(cls, cursor, *args, **kwargs)
+#        cursor.close()
+#        cls._connection.commit()
+#
+#        return res
+#
+#    return wrapper
+#
+#
+#class Storage:
+#    _connection = pg8000.connect(user='radionica', password='P4ss')
+#
+#    @classmethod
+#    @uses_db
+#    def save_tweet(cls, tweet):
+#        t = Tweet(tweet)
+#        cls._tweets[t.id] = t
+#
+#        return json.dumps(t.__dict__)
+#
+#    @classmethod
+#    @uses_db
+#    def get_all(cls, cursor):
+#        cursor.execute('SELECT id, name, tweet FROM tweets')
+#
+#        return json.dumps([Tweet(*tweet).jsoned() for tweet in cursor.fetchall()])
+#
+#    @classmethod
+#    def get_tweet(cls, cursor, id):
+#        cursor.execute(
+#            'SELECT id, name, tweet FROM tweets WHERE id = {id}'.format(id))
+#
+#        res = cursor.fetchone()
+#        if res:
+#            return Tweet(*res).jsoned()
+#        else:
+#            return None
+#
+#    @classmethod
+#    def delete_tweet(cls, id):
+#        tweet_id = cls._tweets.pop(id, None)
+#
+#        if tweet_id or tweet_id == 0:
+#            return True
+#        else:
+#            return False
+#
