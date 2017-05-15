8c827d5985329c387898e63993f95e7c4c3133ac
Nikola Pavlović <nzp@riseup.net>
2017-05-15 02:50:21 +0200

    Add fabfile.py.

---
+++ fabfile.py
@@ -0,0 +1,130 @@
+from fabric.api import env
+from fabric.api import local
+from fabric.api import run
+from fabric.api import settings
+
+import local_fabric_settings as fs
+
+
+env.hosts = [fs.HOST]
+env.port = fs.PORT
+env.user = fs.USER
+
+repo = fs.DOCKER_REPO
+
+net = 'seventweets'
+vol = 'seventweets-postgres-data'
+
+pg_docker_name = 'seventweets-postgres'
+pg_user = fs.PG_USER
+pg_pass = fs.PG_PASS
+
+app_docker_name = 'seventweets-app'
+
+nginx_docker_name = 'seventweets-nginx'
+nginx_conf = fs.NGINX_CONF
+
+
+def test():
+    local('PYTHONPATH=. pytest --cov=seventweets')
+
+
+def build():
+    local('docker build -t {} .'.format(repo))
+
+
+def push():
+    local('docker push {}'.format(repo))
+
+
+def pull():
+    run('docker pull {}'.format(repo))
+
+
+def create_network():
+    with settings(warn_only=True):
+        run('docker network create {}'.format(net))
+
+
+def create_volume():
+    with settings(warn_only=True):
+        run('docker volume create {}'.format(vol))
+
+
+def run_postgres():
+    run('docker run -d ' +
+       '--name {} '.format(pg_docker_name) +
+       '--net {} '.format(net) +
+       '--restart unless-stopped ' +
+       '-e POSTGRES_USER={} '.format(pg_user) +
+       '-e POSTGRES_PASSWORD={} '.format(pg_pass) +
+       '-v {}:/var/lib/postgresql/data '.format(vol) +
+        'postgres:9.6')
+
+
+def stop_postgres():
+    run('docker stop {}'.format(pg_docker_name))
+
+
+def start_postgres():
+    run('docker start {}'.format(pg_docker_name))
+
+
+def restart_postgres():
+    stop_postgres()
+    start_postgres()
+
+
+def run_app():
+    run('docker run -d ' +
+       '--name {} '.format(app_docker_name) +
+       '--net {} '.format(net) +
+       '--restart unless-stopped ' +
+       '-e ST_PG_USER={} '.format(pg_user) +
+       '-e ST_PG_PASS={} '.format(pg_pass) +
+       '-e ST_PG_HOST={} '.format(pg_docker_name) +
+       '{}'.format(repo))
+
+
+def stop_app():
+    run('docker stop {}'.format(app_docker_name))
+
+
+def start_app():
+    run('docker start {}'.format(app_docker_name))
+
+
+def restart_app():
+    stop_app()
+    start_app()
+
+
+def run_nginx():
+    run('docker run -d ' +
+       '--name {} '.format(nginx_docker_name) +
+       '--net {} '.format(net) +
+       '--restart unless-stopped ' +
+       '-v {}:/etc/nginx/conf.d/default.conf '.format(nginx_conf) +
+       '-p 9000:80 ' +
+       'nginx')
+
+
+def stop_nginx():
+    run('docker stop {}'.format(nginx_docker_name))
+
+
+def start_nginx():
+    run('docker start {}'.format(nginx_docker_name))
+
+
+def restart_nginx():
+    stop_nginx()
+    start_nginx()
+
+
+def deploy():
+    build()
+    push()
+    pull()
+    restart_app()
+
--- seventweets/config.py
+++ seventweets/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    DB_CONFIG = dict(user=os.environ.get('PG_USER'),
-                     password=os.environ.get('PG_PASS'),
-                     host=os.environ.get('PG_HOST'))
+    DB_CONFIG = dict(user=os.environ.get('ST_PG_USER'),
+                     password=os.environ.get('ST_PG_PASS'),
+                     host=os.environ.get('ST_PG_HOST'))
 
     NAME = 'nzp'
