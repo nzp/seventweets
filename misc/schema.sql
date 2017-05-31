beb39f25b4b3ad52fb66ff41b719812e53ad95d7
Nikola Pavlović <nzp@riseup.net>
2017-06-01 01:48:57 +0200

    Change DB tweet.content length to 500 characters.

--- misc/schema.sql
+++ misc/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS tweet CASCADE;
 CREATE TABLE tweet (
 	id SERIAL PRIMARY KEY,
 	node_name VARCHAR(10) NOT NULL,
-	content VARCHAR(200),
+	content VARCHAR(500),
 	pub_datetime TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
 	rt BOOLEAN DEFAULT FALSE,
 	rt_origin_name VARCHAR(100),
